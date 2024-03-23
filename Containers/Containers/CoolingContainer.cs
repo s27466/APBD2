75fd524c0c44906dfb0dfeb8f5eb6085bfde585b
s24666 <s27466@pjwstk.edu.pl>
2024-03-23 09:46:00 +0100

    dodano cooling container

--- Containers/Containers/CoolingContainer.cs
+++ Containers/Containers/CoolingContainer.cs
@@ -1,6 +1,47 @@
-﻿public class CoolingContainer : DefaultContainer
+﻿using System.Runtime.CompilerServices;
+
+public class CoolingContainer : DefaultContainer
 {
     private static int serialCounter = 1;
     private List<Product> possibleProducts = new List<Product>();
+    private string productName;
+    private double temperature;
 
+    public CoolingContainer(float mass, float height, float depth, float maxCapacity, List<Product> possibleProducts, string productName, double temperature)
+    {
+        this.mass = mass;
+        this.height = height;
+        this.depth = depth;
+        this.maxCapacity = maxCapacity;
+        this.possibleProducts = possibleProducts;
+        this.productName = productName;
+
+        if (!IsValid())
+        {
+            throw new InvalidOperationException("Invalid product name or temperature.");
+        }
+
+        this.serialNumber = "KON-C-" + serialCounter;
+        serialCounter++;
+        this.temperature = temperature;
+    }
+
+    private bool IsValid()
+    {
+        foreach (Product product in possibleProducts)
+        {
+            if (product.getName() == productName && this.temperature < product.getTemp())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void getInfo()
+    {
+        Console.WriteLine("container mass: " + mass + "\ncontainer height: " + height + "\ncontainer depth: " + depth +
+                          "\ncontainer serial number: " + serialNumber + "\ncontainer max capacity: " + maxCapacity + "\ntemperature: " + temperature + "\ncargo type: " + productName +
+                          "\ncargo mass: " + cargoMass);
+    }
 }
--- Containers/Containers/Program.cs
+++ Containers/Containers/Program.cs
@@ -5,15 +5,32 @@ LiquidContainer waterContainer1 = new LiquidContainer(10,50,45,50,true);
 LiquidContainer waterContainer2 = new LiquidContainer(15,60,55,70,false);
 
 GasContainer heliumContainer = new GasContainer(10,50,45,50,3000);
-CoolingContainer bananaContainer = new CoolingContainer();
+
+List<Product> productList = new List<Product>
+{
+    new Product("Milk", 4),
+    new Product("Egg", 2),
+    new Product("Beef", 1),
+    new Product("Fruit Juice", 5),
+    new Product("Cheese", 6),
+    new Product("Carrot", 3),
+    new Product("Yogurt", 8),
+    new Product("Banana", 7),
+    new Product("Fish", 0),
+    new Product("Ice Cream", -10)
+};
+
+CoolingContainer bananaContainer = new CoolingContainer(12,70,60,90,productList,"Banana",7);
 
 waterContainer1.addCargo(30);
 waterContainer2.addCargo(40);
+bananaContainer.addCargo(80);
 
 heliumContainer.addCargo(20);
 
 waterContainer1.getInfo();
 waterContainer2.getInfo();
 heliumContainer.getInfo();
+bananaContainer.getInfo();
 
 waterContainer2.removeCargo();
